67e4e6d5a3c050134a51f25763c2290e9831bd5e
Federico467 <federico.buzzini@studenti.unitn.it>
2024-02-17 10:44:57 +0100

    initializing repo

---
+++ Network.py
@@ -0,0 +1,9 @@
+#file to ceate mininet topology and run the network
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink
+
