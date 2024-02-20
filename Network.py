642bd6de17f4a2ed09b8a11edc8176fd26e2981a
Federico467 <federico.buzzini@studenti.unitn.it>
2024-02-20 10:13:18 +0100

    Service done

--- Network.py
+++ Network.py
@@ -1,9 +1,65 @@
+#!/usr/bin/python3
+
 #file to ceate mininet topology and run the network
 
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink
 
+# Network Slicing  
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+
+        # # Create a topology with 5 switches and 7 hosts
+        # # Add hosts and switches
+        for i in range(1, 8):
+            h = self.addHost('h%s' % (i+1))
+        
+
+        for i in range(1, 6):
+            sconfig = {"dpid": "%016x" % (i + 1)}
+            s = self.addSwitch('s%d' % (i + 1), **sconfig)
+        
+        # Add links, hosts to switches have infinite bandwith
+        self.addLink('h1', 's1', bw=1000)
+        self.addLink('h2', 's2', bw=1000)
+        self.addLink('h3', 's3', bw=1000)
+        self.addLink('h4', 's4', bw=1000)
+        self.addLink('h5', 's4', bw=1000)
+        self.addLink('h6', 's5', bw=1000)
+        self.addLink('h7', 's5', bw=1000)
+
+        # Add links between switches
+        self.addLink('s1', 's2', bw=3)
+        self.addLink('s2', 's3', bw=3)
+        self.addLink('s3', 's4', bw=5)
+        self.addLink('s4', 's5', bw=1)
+        self.addLink('s5', 's1', bw=2)
+        self.addLink('s5', 's2', bw=5)
+        self.addLink('s4', 's1', bw=4)
+
+def runNetwork():
+    # Create a network with a remote controller
+    net = Mininet(
+        topo=MyTopo(), 
+        link=TCLink,
+        controller=RemoteController, #da controllare ordine paramentri
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,)
+    net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
+    net.build()
+    net.start()
+    info('** Running CLI\n')
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info') # set Mininet log level
+    runNetwork()
+```
---
+++ TopologySlicing.py
@@ -0,0 +1,89 @@
+
+
+from ryu,base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+
+class NetworkSlicing(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(NetworkSlicing, self).__init__(*args, **kwargs)
+
+        # Define the port to port mapping
+      self.portToPortSlicing = {
+            1: {1: 2, 2: 1, 4: 3, 3: 4},
+            2: {2: 1, 1: 2, 1: 3, 3: 1},
+            3: {1: 2, 2: 1},
+            4: {1: 5, 5: 1, 2: 3, 3: 2},
+            5: {2: 5, 5: 2, 1: 4, 4: 1}
+        }
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+
+
+
+    def send_packet(self, datapath, in_port, actions, msg):
+        data = None
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Construct packet_out message and send it.
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+        
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        
+        datapath.send_msg(out)
+
+
+
+
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        
+        # Construct flow_mod message and send it.
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+
+        msg = ev.msg
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+        dpid = datapath.id
+
+        out_port = self.portToPortSlicing[dpid][in_port]
+        actions = [parser.OFPActionOutput(out_port)]
+        match = parser.OFPMatch(in_port=in_port)
+
+        self.add_flow(datapath, 1, match, actions)  # Add flow entry to the switch
+        self.send_packet(datapath, in_port, actions, msg) # Send the packet out to the switch
+
+
+
+```
+
+# Path: NetworkSlicing.py
