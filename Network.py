b05372625fbc982430a06776f016420ed1f2d895
Federico467 <federico.buzzini@studenti.unitn.it>
2024-02-20 11:44:20 +0100

    s

--- Network.py
+++ Network.py
@@ -18,12 +18,23 @@ class MyTopo(Topo):
         linkHosts=dict(bw=1000)
 
         # Create a topology with 5 switches and 7 hosts
-        for i in range(1, 8):
-            self.addHost('h%s' % (i+1))
 
-        for i in range(1, 6):
+        for i in range(0, 5):
             sconfig = {"dpid": "%016x" % (i + 1)}
             self.addSwitch('s%d' % (i + 1), **sconfig)
+
+        for i in range(0, 7):
+            self.addHost('h%s' % (i+1))
+
+        
+        # Add links between switches
+        self.addLink('s1', 's2', **linkSwitches)
+        self.addLink('s2', 's3', **linkSwitches)
+        self.addLink('s3', 's4', **linkSwitches)
+        self.addLink('s4', 's5', **linkSwitches)
+        self.addLink('s5', 's1', **linkSwitches)
+        self.addLink('s5', 's2', **linkSwitches)
+        self.addLink('s4', 's1', **linkSwitches)
         
         # Add links, hosts to switches have infinite bandwith
         self.addLink('h1', 's1', **linkHosts)
@@ -34,14 +45,7 @@ class MyTopo(Topo):
         self.addLink('h6', 's5', **linkHosts)
         self.addLink('h7', 's5', **linkHosts)
 
-        # Add links between switches
-        self.addLink('s1', 's2', **linkSwitches)
-        self.addLink('s2', 's3', **linkSwitches)
-        self.addLink('s3', 's4', **linkSwitches)
-        self.addLink('s4', 's5', **linkSwitches)
-        self.addLink('s5', 's1', **linkSwitches)
-        self.addLink('s5', 's2', **linkSwitches)
-        self.addLink('s4', 's1', **linkSwitches)
+        
 
 def runNetwork():
     # Create a network with a remote controller
