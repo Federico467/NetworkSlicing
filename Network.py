9661c325fa373374bb435cfd593a055f6342b763
Federico467 <federico.buzzini@studenti.unitn.it>
2024-02-21 09:12:29 +0100

    s

--- Network.py
+++ Network.py
@@ -1,71 +1,60 @@
 #!/usr/bin/python3
 
-#file to ceate mininet topology and run the network
-
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
 from mininet.link import TCLink
 
-# Network Slicing  
-class MyTopo(Topo):
+
+class NetworkSlicingTopo(Topo):
     def __init__(self):
+        # Initialize topology
         Topo.__init__(self)
 
-        linkSwitches={}
-        linkHosts={}
+        # Create template host, switch, and link
+        host_config = dict(inNamespace=True)
+        http_link_config = dict(bw=1)
+        video_link_config = dict(bw=10)
+        host_link_config = dict()
 
-        # Create a topology with 5 switches and 7 hosts
-        for i in range(0, 7):
-            self.addHost('h%d' % (i+1))
-
-        for i in range(0, 5):
+        # Create switch nodes
+        for i in range(4):
             sconfig = {"dpid": "%016x" % (i + 1)}
-            self.addSwitch('s%d' % (i + 1), **sconfig)
+            self.addSwitch("s%d" % (i + 1), **sconfig)
 
-        
-        # Add links, hosts to switches have infinite bandwith
-        self.addLink('s1', 'h1', port1=1, port2=6, **linkHosts)
-        self.addLink('s2', 'h2', port1=1, port2=6, **linkHosts)
-        self.addLink('s3', 'h3', port1=1, port2=6, **linkHosts)
-        self.addLink('s4', 'h4', port1=1, port2=6, **linkHosts)
-        self.addLink('s4', 'h5', port1=2, port2=6, **linkHosts)
-        self.addLink('s5', 'h6', port1=1, port2=6, **linkHosts)
-        self.addLink('s5', 'h7', port1=2, port2=6, **linkHosts)
-        
-        # Add links between switches
-        self.addLink('s1', 's2', port1=2, port2=2, **linkSwitches)
-        self.addLink('s1', 's5', port1=3, port2=4, **linkSwitches)
-        self.addLink('s1', 's4', port1=4, port2=3, **linkSwitches)
-        self.addLink('s2', 's3', port1=3, port2=2, **linkSwitches)
-        self.addLink('s2', 's5', port1=4, port2=3, **linkSwitches)
-        self.addLink('s3', 's4', port1=3, port2=4, **linkSwitches)
-        self.addLink('s4', 's5', port1=5, port2=5, **linkSwitches)
-        
-       
+        # Create host nodes
+        for i in range(4):
+            self.addHost("h%d" % (i + 1), **host_config)
 
-        
+        # Add switch links
+        self.addLink("s1", "s2", **video_link_config)
+        self.addLink("s2", "s4", **video_link_config)
+        self.addLink("s1", "s3", **http_link_config)
+        self.addLink("s3", "s4", **http_link_config)
 
-def runNetwork():
-    # Create a network with a remote controller
+        # Add host links
+        self.addLink("h1", "s1", **host_link_config)
+        self.addLink("h2", "s1", **host_link_config)
+        self.addLink("h3", "s4", **host_link_config)
+        self.addLink("h4", "s4", **host_link_config)
+
+
+topos = {"networkslicingtopo": (lambda: NetworkSlicingTopo())}
+
+if __name__ == "__main__":
+    topo = NetworkSlicingTopo()
     net = Mininet(
-        topo=MyTopo(),
+        topo=topo,
         switch=OVSKernelSwitch,
         build=False,
         autoSetMacs=True,
         autoStaticArp=True,
         link=TCLink,
     )
-    net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
+    controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    net.addController(controller)
     net.build()
     net.start()
-    info('** Running CLI\n')
     CLI(net)
     net.stop()
-
-if __name__ == '__main__':
-    setLogLevel('info') # set Mininet log level
-    runNetwork()
-
--- TopologySlicing.py
+++ TopologySlicing.py
@@ -1,24 +1,22 @@
-
-
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
-class NetworkSlicing(app_manager.RyuApp):
+
+class TrafficSlicing(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(NetworkSlicing, self).__init__(*args, **kwargs)
+        super(TrafficSlicing, self).__init__(*args, **kwargs)
 
-        # Define the port to port mapping
-        self.portToPortSlicing = {
-            1: {1: 2, 2: 1, 4: 3, 3: 4},
-            2: {2: 1, 1: 2, 1: 3, 3: 1},
-            3: {2: 1, 1: 2},
-            4: {1: 5, 5: 1, 2: 3, 3: 2},
-            5: {2: 5, 5: 2, 1: 4, 4: 1}
+        # out_port = slice_to_port[dpid][in_port]
+        self.slice_to_port = {
+            1: {1: 3, 3: 1, 2: 4, 4: 2},
+            4: {1: 3, 3: 1, 2: 4, 4: 2},
+            2: {1: 2, 2: 1},
+            3: {1: 2, 2: 1},
         }
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -27,63 +25,49 @@ class NetworkSlicing(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # install table-miss flow entry
+        # install the table-miss flow entry.
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
+        ]
         self.add_flow(datapath, 0, match, actions)
-
-
-
-
-    def send_packet(self, datapath, in_port, actions, msg):
-        data = None
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        # Construct packet_out message and send it.
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-        
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        
-        datapath.send_msg(out)
-
-
-
-
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        
-        # Construct flow_mod message and send it.
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(
+            datapath=datapath, priority=priority, match=match, instructions=inst
+        )
         datapath.send_msg(mod)
+
+    def _send_package(self, msg, datapath, in_port, actions):
+        data = None
+        ofproto = datapath.ofproto
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(out)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-
         msg = ev.msg
         datapath = msg.datapath
-        parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
         dpid = datapath.id
 
-        out_port = self.portToPortSlicing[dpid][in_port]
-        actions = [parser.OFPActionOutput(out_port)]
-        match = parser.OFPMatch(in_port=in_port)
+        out_port = self.slice_to_port[dpid][in_port]
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+        match = datapath.ofproto_parser.OFPMatch(in_port=in_port)
 
-        self.add_flow(datapath, 1, match, actions)  # Add flow entry to the switch
-        self.send_packet(datapath, in_port, actions, msg) # Send the packet out to the switch
-
-
-
-
-
-# Path: NetworkSlicing.py
+        self.add_flow(datapath, 1, match, actions)
+        self._send_package(msg, datapath, in_port, actions)
